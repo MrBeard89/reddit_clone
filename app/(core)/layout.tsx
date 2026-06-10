07dac69d52147d9597411b36d113df09c496a8fb
Norbert Kovács <k.n.89@outlook.hu>
2026-06-10 11:51:53 +0200

    queries.ts setup

--- app/(core)/layout.tsx
+++ app/(core)/layout.tsx
@@ -1,12 +1,16 @@
-import LeftSidebar from '@/components/layout/left-sidebar'
+import { LeftSidebar } from '@/components/layout/left-sidebar'
 import { Navbar } from '@/components/layout/navbar'
+import { getSessionUser } from '@/lib/auth/auth'
+import { tagPostCounts } from '@/lib/db/queries'
 
 export default async function CoreGroupLayout({ children }: { children: React.ReactNode }) {
+  const user = await getSessionUser()
+  const tags = await tagPostCounts()
   return (
     <>
       <Navbar />
       <div className='mx-auto flex max-w-300 gap-8 px-4 pb-16 pt-2'>
-        <LeftSidebar />
+        <LeftSidebar showCta={!user} tagsWithCounts={tags} />
         <div className='min-w-0 flex-1'>{children}</div>
       </div>
     </>
